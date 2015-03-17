84d7a9e2c39cd757f64123c82942a348991266b6
Ann <anna.poddubnaya@gmail.com>
2015-03-17 14:54:42 +0200

    menu bar

--- src/ColorDetector.java
+++ src/ColorDetector.java
@@ -1,60 +1,58 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ColorDetector{
     private JFrame panel;
-    private JPanel menu = new JPanel(new BorderLayout());
+    private JMenuBar menu = new JMenuBar();
     private JButton pickingState = new JButton("choose point");
     private ImageIcon image = new ImageIcon("angry_beaver-1.gif");
 
     public ColorDetector(){
-    	JButton file = new JButton("file");
-    	final JPanel fileMenu = new JPanel(new BorderLayout());
-    	JButton newPicture = new JButton("make new");
-    	JButton openPicture = new JButton("open");
-    	JButton exit = new JButton("exit");
+    	JMenu fileMenu = new JMenu("file");
+    	JMenuItem newPicture = new JMenuItem("make new");
+    	JMenuItem openPicture = new JMenuItem("open");
+    	JMenuItem exit = new JMenuItem("exit");
     	fileMenu.add(newPicture);
     	fileMenu.add(openPicture);
     	fileMenu.add(exit);
-    	fileMenu.setVisible(false);
-    	file.addActionListener(new ActionListener(){
+    	menu.add(fileMenu);
+    	
+    	JMenu help = new JMenu("help");
+    	JMenuItem showHelp = new JMenuItem("help..");
+    	showHelp.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-	        	fileMenu.setVisible(true);
-			}        	
-        });
-
-    	JButton help = new JButton("help");
-    	help.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				JFrame helpFrame = new JFrame();
-				JLabel helpLable = new JLabel(" we will hepl you! ");
-				helpFrame.add(helpLable);
-				helpFrame.pack();
-				helpFrame.setVisible(true);
+                String message = "<html><body width=300>" +
+                		"<h1>DO NOT WORRY!</h1>" +
+                    "<p>we will help you!";
+                JOptionPane.showMessageDialog(null, message, "HELP", JOptionPane.INFORMATION_MESSAGE);
 			}	
     	});
-    	JButton about = new JButton("about");
+    	help.add(showHelp);
+    	
+    	JMenuItem about = new JMenuItem("about..");
     	about.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				JFrame aboutFrame = new JFrame();
-				JLabel aboutLable = new JLabel(" Let stop talk about us! How are you?");
-				aboutFrame.add(aboutLable);
-				aboutFrame.pack();
-				aboutFrame.setVisible(true);
+                String message = "<html><body width=300>" +
+                    "<p>Let stop talk about us! How are you?";
+                JOptionPane.showMessageDialog(null, message, "ABOUT", JOptionPane.INFORMATION_MESSAGE);
 			}	
     	});
-    	menu.add(file, BorderLayout.WEST);
+    	help.add(about);
     	menu.add(help);
-    	menu.add(about, BorderLayout.EAST);
+    	
     	
     	panel = new JFrame();
         panel.add(pickingState, BorderLayout.SOUTH);   	

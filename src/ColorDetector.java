0373d0c00394f3ddb7d76e4852130bb0a9331d4d
liliya-nikolenko <liliia.nikolenko@gmail.com>
2015-03-25 14:07:03 +0200

    Change class for image, add open file dialog

--- src/ColorDetector.java
+++ src/ColorDetector.java
@@ -1,9 +1,4 @@
-import java.awt.BorderLayout;
-import java.awt.Cursor;
-import java.awt.FlowLayout;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -11,31 +6,40 @@ import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
-public class ColorDetector{
-    private JFrame panel;
+public class ColorDetector extends JFrame {
     private JMenuBar menu = new JMenuBar();
     private JButton pickingState = new JButton("choose point");
-    private myImage image = new myImage("angry_beaver-1.gif");
+	private ImagePanel panel = new ImagePanel();
 
     public ColorDetector(){
-    	JMenu fileMenu = new JMenu("file");
+		super();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1024, 768);
+		this.setTitle("Color detector");
+
+		JMenu fileMenu = new JMenu("file");
     	JMenuItem newPicture = new JMenuItem("make new");
     	JMenuItem openPicture = new JMenuItem("open");
     	JMenuItem exit = new JMenuItem("exit");
-    	fileMenu.add(newPicture);
+
+		openPicture.addActionListener(new ActionListener (){
+			@Override
+			public void actionPerformed(ActionEvent ae){
+				openFileDialog();
+			}
+		});
+
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		fileMenu.add(newPicture);
     	fileMenu.add(openPicture);
     	fileMenu.add(exit);
     	menu.add(fileMenu);
@@ -68,32 +72,54 @@ public class ColorDetector{
     	pickingState.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				panel.getRootPane().setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+				getRootPane().setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 			}
     	});
-    	
-    	
-    	panel = new JFrame();
-        panel.add(menu, BorderLayout.NORTH);
-        panel.add(pickingState, BorderLayout.SOUTH);   	
 
-        //this part is not nice enough
-        JLabel label = new JLabel("", image, JLabel.CENTER);
-        panel.add(label, BorderLayout.WEST);
-        
-        panel.pack();
-        panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel.setVisible(true);
-    	
-    }
-    
-    private class myImage extends ImageIcon implements MouseListener{
 
-    	public myImage(String pictureLocation){
-    		super(pictureLocation);
+        this.add(menu, BorderLayout.NORTH);
+		this.add(pickingState, BorderLayout.SOUTH);
+		this.add(panel, BorderLayout.CENTER);
+	}
+
+	void openFileDialog(){
+		FileDialog openFileDialog = new FileDialog(this, "Open file");
+		openFileDialog.setVisible(true);
+		panel.setImage(openFileDialog.getDirectory() + "/" + openFileDialog.getFile());
+
+		this.validate();
+		this.repaint();
+	}
+
+    private class ImagePanel extends JPanel implements MouseListener{
+		private BufferedImage image;
+
+		public ImagePanel() {
+			try {
+				image = ImageIO.read(new File("angry_beaver-1.gif"));
+			} catch (IOException ignore) { /* NOP */ }
+		}
+
+		public ImagePanel(String pictureLocation){
+			try {
+				image = ImageIO.read(new File(pictureLocation));
+			} catch (IOException ignore) { /* NOP */ }
     	}
-    	
+
+		void setImage(String pictureLocation){
+			try {
+				image = ImageIO.read(new File(pictureLocation));
+			} catch (IOException ignore) { /* NOP */ }
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(image, 0, 0, null);
+		}
+
+
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			// TODO Auto-generated method stub
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-    	ColorDetector ColorDet = new ColorDetector();
+        final ColorDetector colorDet = new ColorDetector();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                colorDet.setVisible(true);
+            }
+        });
     }
 }
 
